4900c402849daa94ddccd5d63fef8ba428723ea9
daniel mercia <danmercia@gmail.com>
2017-10-27 20:14:05 -0400

    added app menu bar

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/mirror.html`);
+
+const menuTemplate = [
+  {
+    label: 'Screen Assistant',
+    submenu: [
+      {
+        label: 'About Screen Assistant'
+      },
+      {
+        label: 'New Screen'
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        }
+      }
+
+    ]
+  }
+];
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
+const menu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(menu);
 });
